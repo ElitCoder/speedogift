43a18e68716677368a778a0d6a5f4aeac6d22cb4
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2019-09-11 22:04:41 +0200

    Check for dependency updates in parallel

--- check_dep.sh
+++ check_dep.sh
@@ -12,46 +12,55 @@ function pull {
         cmake -G Ninja ..
         ninja
         sudo ninja install
-        cd ..
     else
         ./build.sh
         sudo make install
     fi
-    cd ..
+    return $?
+}
+
+function parse_dep {
+    repo_name=$1
+    url=$2
+    if [ ! -d ${repo_name[1]} ]; then
+        echo "can't find repo dir ${repo_name[1]}, resetting"
+        pull $url
+        return $?
+    fi
+    cd ${repo_name[1]}
+    if [ ! -d .git ]; then
+        echo "not an git repository ${repo_name[1]}, resetting"
+        cd ..
+        rm -rf ${repo_name[1]}
+        pull $url
+        return $?
+    fi
+    git fetch
+    if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]; then
+        echo "old version for ${repo_name[1]}, resetting"
+        cd ..
+        rm -rf ${repo_name[1]}
+        pull $url
+        return $?
+    fi
 }
 
 function check_deps {
-    local res=0
+    local FAIL=0
     cd dep
     for i in ${deps[@]}; do
-        repo_name=(${i//// })
-        if [ ! -d ${repo_name[1]} ]; then
-            echo "can't find repo dir ${repo_name[1]}, resetting"
-            pull $i
-            res=1
-            continue
-        fi
-        cd ${repo_name[1]}
-        if [ ! -d .git ]; then
-            echo "not an git repository ${repo_name[1]}, resetting"
-            cd ..
-            rm -rf ${repo_name[1]}
-            pull $i
-            res=1
-            continue
-        fi
-        git fetch
-        if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]; then
-            echo "old version for ${repo_name[1]}, resetting"
-            cd ..
-            rm -rf ${repo_name[1]}
-            pull $i
-            res=1
-            continue
-        fi
-        cd ..
+        local repo_name=(${i//// })
+        parse_dep $repo_name $i &
     done
-    return $res
+    for job in `jobs -p`; do
+        wait $job || let "FAIL+=1"
+    done
+
+    if [ "$FAIL" == "0" ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 if [ ! -d "dep" ]; then
