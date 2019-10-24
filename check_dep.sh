cacf99ba3062ddcc41498919b1f3b4bf5405ed98
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2019-10-24 20:35:54 +0200

    Rewrite dependency check to be POSIX compatible
    Avoid using extensions like arrays and double square brackets for
    evaluation.

--- check_dep.sh
+++ check_dep.sh
@@ -1,71 +1,50 @@
-deps=(jarro2783/cxxopts ElitCoder/ncnet gabime/spdlog)
+DEPS='jarro2783/cxxopts gabime/spdlog ElitCoder/ncnet'
 
-function pull {
-    local i=$1
-    git clone --depth=1 https://github.com/$i
-    repo_name=(${i//// })
-    cd ${repo_name[1]}
-    if [ -f CMakeLists.txt ]; then
-        echo "Using CMake for building"
-        mkdir -p build && cd build
-        cmake -G Ninja ..
-        ninja
-        sudo ninja install
-    else
-        echo "Don't know how to build ${repo_name[1]}!"
-        return 1
-    fi
-    return $?
+reset_dep() {
+    echo Resetting dependency $1
+    local repo;
+    repo=${1#*/}
+    rm -rf $repo
+    git clone https://github.com/$1.git || { echo Failed to clone git repository $1, skipping; return 1; }
+    echo Building dependency
+    cd $repo
+    [ -f CMakeLists.txt ] || { echo Only CMake is supported for now, skipping; return 1; }
+    # Build using CMake
+    mkdir -p build && cd build
+    cmake -G Ninja .. || { echo Failed to configure CMake, skipping; return 1; }
+    ninja || { echo Failed to build, skipping; return 1; }
+    sudo ninja install || { echo Failed to install, skipping; return 1; }
 }
 
-function parse_dep {
-    repo_name=$1
-    url=$2
-    if [ ! -d ${repo_name[1]} ]; then
-        echo "Dependency ${repo_name[1]} was not found, downloading..."
-        pull $url
-        return $?
-    fi
-    cd ${repo_name[1]}
-    if [ ! -d .git ]; then
-        echo "Dependency ${repo_name[1]} exists but .git is missing, resetting..."
-        cd ..
-        rm -rf ${repo_name[1]}
-        pull $url
-        return $?
-    fi
-    git fetch
-    if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]; then
-        echo "Dependency ${repo_name[1]} has a newer HEAD, resetting..."
-        cd ..
-        rm -rf ${repo_name[1]}
-        pull $url
-        return $?
-    fi
+check_dep() {
+    echo Checking dependency $1
+    local repo;
+    repo=${1#*/}
+    # Check if repository exists and contains git information
+    [ -d $repo ] || { echo Dependency directory does not exist, resetting; return 1; }
+    [ -d $repo/.git ] || { echo Not a valid git repository, resetting; return 1; }
+    cd $repo
+    # Update git information
+    echo Fetching new git information
+    git fetch || { echo Failed to download git information, skipping; return 1; }
+    # Check HEAD vs origin
+    local head;
+    head=$(git rev-parse HEAD)
+    local origin;
+    origin=$(git rev-parse @{u})
+    [ $head = $origin ] || { echo HEAD is not at the same point as remote, resetting; return 1; }
+    echo Dependency $1 is updated, continuing
 }
 
-function check_deps {
-    local FAIL=0
-    cd dep
-    for i in ${deps[@]}; do
-        local repo_name=(${i//// })
-        parse_dep $repo_name $i &
-    done
-    for job in `jobs -p`; do
-        wait $job || let "FAIL+=1"
-    done
+CURRENT=$(pwd)
+[ -d dep ] || echo Creating \'dep\' directory; mkdir -p dep
 
-    if [ "$FAIL" == "0" ]; then
-        return 0
-    else
-        return 1
-    fi
-}
+for dep in $DEPS; do
+    cd $CURRENT/dep
+    # Check git information
+    check_dep $dep && continue
+    # Reset
+    reset_dep $dep || { echo Failed to reset, aborting; exit 1; }
+done
 
-if [ ! -d "dep" ]; then
-    mkdir dep
-fi
-
-# Otherwise, check
-check_deps
-exit $?
+cd $CURRENT
